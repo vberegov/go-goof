205398fc236fc774069bb8508e0065a4162b40fc
Philippe  Stemberger <pstember@gmail.com>
2021-03-01 15:27:50 +0000

    updated example

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 import (
 	"fmt"
-	"github.com/pstember/go-goof/hello"
-    "go-goof/handlers"
-    "github.com/gogs/gogs/pkg/tool"
-    "github.com/gin-gonic/gin"
+	"golang.org/x/text/encoding/unicode"
 )
 func main() {
-		fmt.Println(tool.MD5(hello.Hello()))
-        r := gin.Default()
-        r.GET("/ping", handlers.Ping)
-        r.Run() // listen and serve on 0.0.0.0:8080
+		myEncoder := unicode.UTF16(unicode.BigEndian, unicode.UseBOM)
+        myEncoder.NewDecoder()        
+        gmt.Println("hello world")
 }
